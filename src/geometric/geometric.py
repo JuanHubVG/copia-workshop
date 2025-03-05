21ef52373cc10c5b7741874424549e05a8f280b9
Juan David Vásquez <gatoaa.1999@gmail.com>
2025-03-05 17:45:33 -0500

    Corrección ejercicios

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -1,5 +1,6 @@
+import math
 class Geometria:
-    
+
     def area_rectangulo(self, base, altura):
         return base*altura
     
@@ -10,7 +11,7 @@ class Geometria:
         return ((radio*radio)*3.141592)
     
     def perimetro_circulo(self, radio):
-        return (2*3.141592(radio))
+        return (2*math.pi)*(radio)
     
     def area_triangulo(self, base, altura):
         return (base * altura)/2
@@ -48,16 +49,16 @@ class Geometria:
     
     
     def volumen_esfera(self, radio):
-        return((4/3)*3.1415*(radio**3))
+        return((4/3)*math.pi*(radio**3))
     
     def area_superficie_esfera(self, radio):
-        return(4*3.1415)*(radio**2)
+        return(4*math.pi)*(radio**2)
     
     def volumen_cilindro(self, radio, altura):
-        return (3.1415*(radio**2)*(altura))
+        return (math.pi*(radio**2)*(altura))
     
     def area_superficie_cilindro(self, radio, altura):
-        return (2*3.1415*(radio**2)) + (((2*3.1415)*radio)*altura)
+        return ((2*math.pi)*(radio**2)) + (((2*math.pi)*radio)*altura)
     
     def distancia_entre_puntos(self, x1, y1, x2, y2):
         return (((x2-x1)**2)+(y2-y1)**2)**0.5
@@ -72,7 +73,7 @@ class Geometria:
         return (y2 - y1, x1 - x2, x2 * y1 - x1 * y2) if x1 != x2 else (1, 0, -x1)
 
     def area_poligono_regular(self, num_lados, lado, apotema):
-        return ((num_lados*lado)*apotema)/2
+        return ((num_lados*lado)*(apotema))/2
     
     def perimetro_poligono_regular(self, num_lados, lado):
         return (num_lados * lado)
